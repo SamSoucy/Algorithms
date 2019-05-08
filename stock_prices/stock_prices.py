de88d3c2ea953adee431f1231ccff610e44bc69a
Samsoucy <newf930@outlook.com>
2019-05-08 19:04:52 -0400

    finished stock prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,29 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  max_profit = -10
+  buy_price = 0
+  sell_price = 0
+
+  change_buy_index = True
+
+  for i in range(0, len(prices) - 1):
+    sell_price = prices[i + 1]
+    if change_buy_index:
+          buy_price = prices[i]
+    if sell_price < buy_price:
+          change_buy_index = True
+          continue
+    else:
+        temp_profit = sell_price - buy_price
+        if temp_profit > max_profit:
+              max_profit = temp_profit
+        change_buy_index = False
+
+  return max_profit
+ 
+
+
 
 
 if __name__ == '__main__':
